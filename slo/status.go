f34c97dbb84d4547623aeb2a9cf0128a4cfac130
Christopher Waldon <ckwaldon@us.ibm.com>
2016-09-21 17:52:54 -0400

    Refactor progress reporting into package
    To track the progress of an upload, one can now simply create a
    slo.Status struct and tell it when you start/stop the upload.
    At any time, you can ask it for a status message for the user
    about the upload.

---
+++ slo/status.go
@@ -0,0 +1,91 @@
+package slo
+
+import (
+	"fmt"
+	"time"
+)
+
+type Manager struct {
+	NeedsUpload       []bool
+	NumberNeedsUpload uint
+}
+
+type Status struct {
+	UploadSize     uint
+	TotalUploads   uint
+	NumberUploaded uint
+	uploadStarted  time.Time
+	uploadDuration time.Duration
+}
+
+func NewStatus(numberUploads, uploadSize uint) *Status {
+	return &Status{
+		UploadSize:     uploadSize,
+		TotalUploads:   numberUploads,
+		NumberUploaded: 0,
+	}
+}
+
+func (s *Status) SetNumberUploads(number uint) {
+	if s.uploadStarted != (time.Time{}) {
+		return
+	}
+	s.TotalUploads = number
+}
+
+func (s *Status) Start() {
+	s.uploadStarted = time.Now()
+}
+
+func (s *Status) Stop() {
+	s.uploadDuration = time.Since(s.uploadStarted)
+}
+
+func (s *Status) UploadComplete() {
+	s.NumberUploaded += 1
+}
+
+func (s *Status) Rate() float64 {
+	if s.uploadStarted == (time.Time{}) {
+		return 0.0
+	} else if s.uploadDuration != (time.Duration(0)) {
+		return float64(s.TotalUploads*s.UploadSize) / float64(s.uploadDuration.Seconds())
+	}
+	elapsed := time.Since(s.uploadStarted)
+	rate := float64(s.NumberUploaded*s.UploadSize) / elapsed.Seconds()
+	return rate
+}
+
+func (s *Status) RateMBPS() float64 {
+	return s.Rate() / 1e6
+}
+
+func (s *Status) TimeRemaining() time.Duration {
+	finishedIn := int(float64((s.TotalUploads-s.NumberUploaded)*s.UploadSize) / s.Rate())
+	timeRemaining := time.Duration(finishedIn) * time.Second
+	return timeRemaining
+}
+
+func (s *Status) PercentComplete() float64 {
+	return float64(s.NumberUploaded) / float64(s.TotalUploads) * 100
+}
+
+func (s *Status) String() string {
+	if s.uploadStarted == (time.Time{}) {
+		return "Upload not started yet"
+	} else if s.uploadDuration != (time.Duration(0)) {
+		return fmt.Sprintf(
+			"Upload finished in %s at approximately %2.2f MB/sec",
+			s.uploadDuration,
+			s.RateMBPS())
+	}
+	return fmt.Sprintf(
+		"%3.2f%% Uploaded\tAverage Upload Speed %03.2f MB/sec\t%s Remaining",
+		s.PercentComplete(),
+		s.RateMBPS(),
+		s.TimeRemaining())
+}
+
+func (s *Status) Print() {
+	fmt.Println(s)
+}
