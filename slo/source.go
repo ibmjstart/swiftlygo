aeeab20e82da04bf5375c94d0f0f234147098703
Christopher Waldon <ckwaldon@us.ibm.com>
2016-09-22 01:04:17 -0400

    Refactor chunk reading logic into package

--- slo/source.go
+++ slo/source.go
@@ -1,13 +1,17 @@
 package slo
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
+const defaultBufferLength = mebibyte
+
 // Source wraps a file to make reading it in chunks easier
 type Source struct {
 	file         *os.File
+	fileSize     uint
 	chunkSize    uint
 	numberChunks uint
 }
@@ -15,8 +19,13 @@ type Source struct {
 // NewSource creates a source out of a file so that it can easily be
 // read in chunks.
 func NewSource(file *os.File, chunkSize, numberChunks uint) *Source {
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
 	return &Source{
 		file:         file,
+		fileSize:     uint(info.Size()),
 		chunkSize:    chunkSize,
 		numberChunks: numberChunks,
 	}
@@ -34,35 +43,59 @@ func (s *Source) ChunkData(chunkNumber uint) ([]byte, uint) {
 	return dataSlice, uint(bytesRead)
 }
 
+// ChunkReader defines a convenient way to read a data chunk
 type ChunkReader struct {
 	file         *os.File
 	startingByte uint
 	bytesRead    uint
 	totalBytes   uint
+	bufferLength uint
 }
 
+// ChunkReader creates a reader for a given chunk number
 func (s *Source) ChunkReader(chunkNumber uint) *ChunkReader {
+	totalBytes := s.chunkSize
+	if chunkNumber+1 == s.numberChunks {
+		totalBytes = s.fileSize % s.chunkSize
+	}
 	return &ChunkReader{
 		file:         s.file,
 		startingByte: s.chunkSize * chunkNumber,
 		bytesRead:    0,
-		totalBytes:   s.chunkSize,
+		totalBytes:   totalBytes,
+		bufferLength: defaultBufferLength,
 	}
 }
 
+// Reset sets the internal state of this ChunkReader back to when it was first created so that the
+// data chunk can be read again.
 func (c *ChunkReader) Reset() {
 	c.bytesRead = 0
 }
 
-func (c *ChunkReader) ReadInto(buffer []byte) uint {
+// HasUnreadData returns whether this ChunkReader has returned all of its data (via the Read() method)
+// or whether it has more. When this returns false, do not call Read().
+func (c *ChunkReader) HasUnreadData() bool {
+	return c.bytesRead < c.totalBytes
+}
+
+// String converts the ChunkReader's current state into a String.
+func (c *ChunkReader) String() string {
+	return fmt.Sprintf("starting: %d\ttotal: %d\tread: %d\tbuffer: %d", c.startingByte, c.totalBytes, c.bytesRead, c.bufferLength)
+}
+
+// Read returns a byte slice of the file chunk's content until c.HasUnreadData() is false.
+// Call it within a loop to get all of the data from this file chunk.
+func (c *ChunkReader) Read() []byte {
+	buffer := make([]byte, c.bufferLength)
 	bufferLength := uint(len(buffer))
 	if bytesRemaining := c.totalBytes - c.bytesRead; bytesRemaining <= bufferLength {
 		bufferLength = bytesRemaining
 	}
 	bytesRead, err := c.file.ReadAt(buffer[:bufferLength], int64(c.startingByte+c.bytesRead))
-	if err != nil {
+	c.bytesRead += uint(bytesRead)
+	if err != nil && err != io.EOF {
 		panic(err)
 	}
-	c.bytesRead += uint(bytesRead)
-	return uint(bytesRead)
+	return buffer[:bufferLength]
 }
