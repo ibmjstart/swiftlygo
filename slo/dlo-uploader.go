f950d2a7d4d12c47831fecd6f562a7d817a2b9e5
Ryan Soley <resoley@us.ibm.com>
2016-09-30 14:24:08 -0400

    Add DLO segment upload functionality

--- slo/dlo-uploader.go
+++ slo/dlo-uploader.go
@@ -11,13 +11,13 @@ import (
 const maxObjectSize uint = 1000 * 1000 * 1000 * 5
 
 type dloUploader struct {
-	connection  swift.Connection
+	connection  *swift.Connection
 	source      *os.File
 	container   string
 	segmentName string
 }
 
-func NewDloUploader(connection swift.Connection, source *os.File, container, segmentName string) *dloUploader {
+func NewDloUploader(connection *swift.Connection, source *os.File, container, segmentName string) *dloUploader {
 	return &dloUploader{
 		connection:  connection,
 		source:      source,
@@ -27,36 +27,46 @@ func NewDloUploader(connection swift.Connection, source *os.File, container, seg
 }
 
 func (d *dloUploader) Upload() error {
+	data, err := readFile(d.source)
+	if err != nil {
+		return err
+	}
+
+	hash := hashSource(data)
+
+	fileCreator, err := d.connection.ObjectCreate(d.container, d.segmentName, true, hash, "", nil)
+	if err != nil {
+		return fmt.Errorf("Failed to create DLO segment: %s", err)
+	}
+
+	_, err = fileCreator.Write(data)
+	if err != nil {
+		return fmt.Errorf("Failed to write DLO segment: %s", err)
+	}
+
+	err = fileCreator.Close()
+	if err != nil {
+		return fmt.Errorf("Failed to close DLO segment: %s", err)
+	}
 
 	return nil
 }
 
-func (d *dloUploader) hashSource() (string, error) {
-	data, err := d.readFile()
-	if err != nil {
-		return "", err
-	}
-
-	hashBytes := md5.Sum(data)
+func hashSource(sourceData []byte) string {
+	hashBytes := md5.Sum(sourceData)
 	hash := hex.EncodeToString(hashBytes[:])
 
-	return hash, nil
+	return hash
 }
 
-func (d *dloUploader) readFile() ([]byte, error) {
-	info, err := d.source.Stat()
+func readFile(sourceFile *os.File) ([]byte, error) {
+	info, err := sourceFile.Stat()
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get source file info: %s", err)
 	}
 
-	file, err := os.Open(d.source)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to open source file: %s", err)
-	}
-	defer file.Close()
-
 	data := make([]byte, info.Size())
-	count, err := file.Read(data)
+	_, err = sourceFile.Read(data)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read source file: %s", err)
 	}
