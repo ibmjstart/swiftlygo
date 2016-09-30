4c50d1a6a9720cf74263b106ba6fc9343ad90673
Christopher Waldon <ckwaldon@us.ibm.com>
2016-09-30 15:19:27 -0400

    Show HTTP response on manifest upload failure

--- slo/manifest-uploader.go
+++ slo/manifest-uploader.go
@@ -55,7 +55,7 @@ func (m *manifestUploader) upload() error {
 	if err != nil {
 		return fmt.Errorf("Error sending manifest upload request: %s", err)
 	} else if response.StatusCode < 200 || response.StatusCode >= 300 {
-		return fmt.Errorf("Failed to upload manifest with status %d", response.StatusCode)
+		return fmt.Errorf("Failed to upload manifest with status %d (response: %v)", response.StatusCode, response)
 	}
 	// Check the returned hash against our locally computed one. We need to strip the quotes off of the sides of the hash first
 	if strings.Trim(response.Header["Etag"][0], "\"") != m.manifest.Etag() {
