f8b81bf5dee2956266d4bba74429ebc4b71b5479
Christopher Waldon <ckwaldon@us.ibm.com>
2016-10-04 16:52:23 -0400

    Implement fake Destination

---
+++ auth/null-dest.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"io"
+)
+
+// NullDestination implements the Destination interface but always returns
+// the zero values of its methods.
+type NullDestination uint8
+
+func NewNullDestination() NullDestination {
+	return NullDestination(0)
+}
+
+type nullWriteCloser uint8
+
+func (n nullWriteCloser) Close() error {
+	return nil
+}
+
+func (n nullWriteCloser) Write(p []byte) (int, error) {
+	return len(p), nil
+}
+
+func (n NullDestination) CreateFile(container, objectName string, checkHash bool, Hash string) (io.WriteCloser, error) {
+	return NullWriteCloser(0), nil
+}
+
+// CreateSLO always returns nil.
+func (s NullDestination) CreateSLO(containerName, manifestName, manifestEtag string, sloManifestJSON []byte) error {
+	return nil
+}
+
+// CreateDLO always returns nil.
+func (s NullDestination) CreateDLO(containerName, manifestName, filenamePrefix string) error {
+	return nil
+}
+
+// FileNames returns an empty string slice and nil.
+func (s NullDestination) FileNames(container string) ([]string, error) {
+	return []string{}, nil
+}
+
+// AuthUrl returns the empty string.
+func (s NullDestination) AuthUrl() string {
+	return ""
+}
+
+// AuthToken returns the empty string.
+func (s NullDestination) AuthToken() string {
+	return ""
+}
