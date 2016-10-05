3e121fcdfcc58f185da6872b66618d1796607b6a
Christopher Waldon <ckwaldon@us.ibm.com>
2016-10-05 11:35:54 -0400

    Test file upload contents

--- slo/slo-uploader_test.go
+++ slo/slo-uploader_test.go
@@ -17,6 +17,7 @@ var _ = Describe("SloUploader", func() {
 		destination auth.Destination = auth.NewNullDestination()
 		tempfile    *os.File
 		err         error
+		fileSize    int64 = 1024
 	)
 
 	BeforeSuite(func() {
@@ -24,8 +25,8 @@ var _ = Describe("SloUploader", func() {
 		if err != nil {
 			Fail(fmt.Sprintf("Unable to create temporary file: %s", err))
 		}
-		//write 1024 random bytes into file
-		for i := 0; i < 1024; i++ {
+		//write random bytes into file
+		for i := 0; i < int(fileSize); i++ {
 			_, err = tempfile.Write([]byte{byte(rand.Int())})
 			if err != nil {
 				Fail(fmt.Sprintf("Unable to write data to temporary file: %s", err))
@@ -79,6 +80,50 @@ var _ = Describe("SloUploader", func() {
 			It("Should return an error", func() {
 				_, err = NewUploader(destination, 10, "container", "object", tempfile, 0, false, ioutil.Discard)
 				Expect(err).Should(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Uploader", func() {
+		var destination *auth.BufferDestination = auth.NewBufferDestination()
+		BeforeEach(func() {
+			tempfile.Seek(0, 0)
+		})
+		Context("Uploading test data", func() {
+			It("Should upload the same data that was in the file", func() {
+				uploader, err := NewUploader(destination, 10, "container", "object", tempfile, 1, false, ioutil.Discard)
+				Expect(err).ShouldNot(HaveOccurred())
+				err = uploader.Upload()
+				Expect(err).ShouldNot(HaveOccurred())
+				fileReadBuffer := make([]byte, fileSize)
+				dataWrittenBuffer := make([]byte, fileSize)
+				tempfile.Seek(0, 0)
+				_, err = tempfile.Read(fileReadBuffer)
+				if err != nil {
+					Fail(fmt.Sprintf("Unable to read data from temporary file: %s", err))
+				}
+				_, err = destination.FileContent.Contents.Read(dataWrittenBuffer)
+				for index, writtenByte := range dataWrittenBuffer {
+					Expect(writtenByte).To(Equal(fileReadBuffer[index]))
+				}
+			})
+			It("Should upload correctly when chunk size is a factor of file size", func() {
+				uploader, err := NewUploader(destination, uint(fileSize/2), "container", "object", tempfile, 1, false, ioutil.Discard)
+				Expect(err).ShouldNot(HaveOccurred())
+				err = uploader.Upload()
+				Expect(err).ShouldNot(HaveOccurred())
+				fileReadBuffer := make([]byte, fileSize)
+				dataWrittenBuffer := make([]byte, fileSize)
+				tempfile.Seek(0, 0)
+				bytesReadFromTempFile, err := tempfile.Read(fileReadBuffer)
+				if err != nil {
+					Fail(fmt.Sprintf("Unable to read data from temporary file: %s", err))
+				}
+				bytesWrittenToDestination, err := destination.FileContent.Contents.Read(dataWrittenBuffer)
+				Expect(bytesWrittenToDestination).To(Equal(bytesReadFromTempFile))
+				for index, writtenByte := range dataWrittenBuffer {
+					Expect(writtenByte).To(Equal(fileReadBuffer[index]))
+				}
 			})
 		})
 	})
