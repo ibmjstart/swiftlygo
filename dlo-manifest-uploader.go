aa362531d84f5ac23d6726f378b8b6fff9d4ccbe
Christopher Waldon <ckwaldon@us.ibm.com>
2016-12-05 11:21:33 -0500

    Document package

--- dlo-manifest-uploader.go
+++ dlo-manifest-uploader.go
@@ -13,6 +13,11 @@ type dloManifestUploader struct {
 	connection      auth.Destination
 }
 
+// NewDloManifestUploader returns an uploader that will create a new DLO.
+// The dloContainer determines where the DLO manifest file is stored, whereas
+// the objectContainer determines which container the DLO will look in for
+// files beginning with the given prefix. This allows you to store the DLO
+// in one container while referencing files within another.
 func NewDloManifestUploader(connection auth.Destination, dloContainer, dloName, objectContainer, prefix string) *dloManifestUploader {
 	return &dloManifestUploader{
 		dloContainer:    dloContainer,
@@ -23,6 +28,7 @@ func NewDloManifestUploader(connection auth.Destination, dloContainer, dloName, 
 	}
 }
 
+// Upload actually performs the upload that creates the DLO.
 func (d *dloManifestUploader) Upload() error {
 	err := d.connection.CreateDLO(d.dloContainer, d.dloName, d.objectContainer, d.prefix)
 	if err != nil {
---
+++ doc.go
@@ -0,0 +1,10 @@
+// swiftlygo makes working with Large Objects in OpenStack Object Storage easier.
+//
+// The auth subpackage provides a convenient abstraction for talking to an
+// Object Storage instance.
+// The slo subpackage implements logic specifically for uploading Static Large
+// Objects.
+//
+// The root swiftlygo package provides functionality for easily creating Dynamic
+// Large Objects.
+package swiftlygo
